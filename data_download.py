628dd382e101e2521fec28a8a237899b0e00a9fa
Rodrigo0730 <rodrigomorales0730@gmail.com>
2024-11-04 19:28:24 +0200

    data starting 2003

--- data_download.py
+++ data_download.py
@@ -1,5 +1,5 @@
 import yfinance as yf
 
-data = yf.download('EURUSD=X', start='2015-01-01', end='2023-01-01', interval='1d')
+data = yf.download('EURUSD=X', start='2004-01-01', end='2024-10-10', interval='1d')
 
 data.to_csv('eur_usd_data.csv', index=True)
